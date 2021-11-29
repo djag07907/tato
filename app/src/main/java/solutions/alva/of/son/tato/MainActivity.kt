1f53544398756d9a140f09360038fa563f0db9f5
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-11-28 22:03:12 -0600

    login base functionality with email and password validations

--- app/src/main/java/solutions/alva/of/son/tato/MainActivity.kt
+++ app/src/main/java/solutions/alva/of/son/tato/MainActivity.kt
@@ -6,25 +6,23 @@ import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import solutions.alva.of.son.tato.databinding.ActivitySignInBinding
+import solutions.alva.of.son.tato.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     //Firebase variables
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ActivitySignInBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //Variable initialization
-        binding = ActivitySignInBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // Initialize Firebase Auth
         auth = Firebase.auth
+
     }
 
-
-    //Login FUNCTION
-    fun 
 }
--- app/src/main/java/solutions/alva/of/son/tato/SignInActivity.kt
+++ app/src/main/java/solutions/alva/of/son/tato/SignInActivity.kt
@@ -1,11 +1,66 @@
 package solutions.alva.of.son.tato
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import solutions.alva.of.son.tato.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    //Firebase variables
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        //Variable initialization
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        //Instantiate Firebase login component
+        binding.signInAppCompatButton.setOnClickListener {
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passwordEditText.text.toString()
+
+            //Login Validations
+            when {
+                mEmail.isEmpty() || mPassword.isEmpty() -> {
+                    Toast.makeText(baseContext, "Correo o contraseña inválida", Toast.LENGTH_SHORT).show()
+                } else -> {
+                    SignIn(mEmail, mPassword)
+                }
+            }
+        }
+    }
+
+
+    //Login FUNCTION
+    private fun SignIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success")
+                    reload()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Correo o contraseña inválida",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun reload() {
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
     }
 }
