c79ff11d3f17fddfbbcc09373bbdb63691f1ed24
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-11-28 21:40:58 -0600

    Firebase authentication initial integration

--- app/src/main/java/solutions/alva/of/son/tato/MainActivity.kt
+++ app/src/main/java/solutions/alva/of/son/tato/MainActivity.kt
@@ -1,11 +1,30 @@
 package solutions.alva.of.son.tato
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import solutions.alva.of.son.tato.databinding.ActivitySignInBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Firebase variables
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Variable initialization
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
+
+
+    //Login FUNCTION
+    fun 
 }
