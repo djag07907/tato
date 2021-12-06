1191557ab2b4bff413a106966db0389aea5d2555
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-12-06 03:15:41 -0600

    Update Profile Image feature

--- app/src/main/java/solutions/alva/of/son/tato/MainActivity.kt
+++ app/src/main/java/solutions/alva/of/son/tato/MainActivity.kt
@@ -2,14 +2,18 @@ package solutions.alva.of.son.tato
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import solutions.alva.of.son.tato.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     //Firebase variables
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
+    private val fileResult = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +40,57 @@ class MainActivity : AppCompatActivity() {
             updateProfile(name)
         }
 
+        binding.profileImageView.setOnClickListener {
+            fileManager()
+        }
+
         updateUI()
     }
 
+    private fun fileManager() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, fileResult)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == fileResult) {
+            if (resultCode == RESULT_OK && data != null) {
+                val uri = data.data
+
+                uri?.let { imageUpload(it) }
+
+            }
+        }
+    }
+
+    private fun imageUpload(mUri: Uri) {
+
+        val user = auth.currentUser
+        val folder: StorageReference = FirebaseStorage.getInstance().reference.child("Users")
+        val fileName: StorageReference = folder.child("img"+user!!.uid)
+
+        fileName.putFile(mUri).addOnSuccessListener {
+            fileName.downloadUrl.addOnSuccessListener { uri ->
+
+                val profileUpdates = userProfileChangeRequest {
+                    photoUri = Uri.parse(uri.toString())
+                }
+
+                user.updateProfile(profileUpdates)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Se realizaron los cambios correctamente.",
+                                Toast.LENGTH_SHORT).show()
+                            updateUI()
+                        }
+                    }
+            }
+        }.addOnFailureListener {
+            Log.i("TAG", "file upload error")
+        }
+    }
 
     private fun updateProfile(name : String) {
         val user = auth.currentUser
