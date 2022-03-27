a291c00066cb529e2a113312580c7f220316bcc2
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2022-03-27 17:05:01 -0600

    recycler view for tech listing

---
+++ app/src/main/java/solutions/alva/of/son/tato/TechListingActivity.kt
@@ -0,0 +1,11 @@
+package solutions.alva.of.son.tato
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TechListingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tech_listing)
+    }
+}
